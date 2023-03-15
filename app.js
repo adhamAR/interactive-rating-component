dd2abf06f84d36557e97d576a618167b5d5dbae3
Adham <zubairadham.ar@gmail.com>
2023-03-16 05:49:37 +0800

    create js function

---
+++ app.js
@@ -0,0 +1,23 @@
+const submit = document.querySelector(".submit");
+const rates = document.querySelectorAll("#rating");
+const beforeRate = document.querySelector(".card__before");
+const afterRate = document.querySelector(".card__after");
+const msg = document.querySelector(".rating-msg");
+
+function showRate() {
+  rates.forEach((rate, index) => {
+    rate.onclick = () => {
+      let selectedRate = index + 1;
+      console.log('You selected ' + selectedRate);
+      submit.onclick = () => {
+        msg.innerHTML = `You selected ${selectedRate} out of 5`;
+        beforeRate.style.display = "none";
+        afterRate.style.display = "grid";
+      }
+      return selectedRate;
+    };
+  });
+}
+
+showRate();
+
